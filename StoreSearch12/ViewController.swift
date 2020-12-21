a2db1c00c9b8d7a34e5c2c3b39ef89475a495ca1
Buck <buck@buckrozelle.com>
2020-12-21 17:03:32 -0500

    Initial Commit

---
+++ StoreSearch12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch12
+//
+//  Created by Buck Rozelle on 12/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
