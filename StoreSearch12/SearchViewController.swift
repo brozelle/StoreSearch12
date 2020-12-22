75e4e0ac6ec9c4d413f9e0266dac17453d14f6f5
Buck <buck@buckrozelle.com>
2020-12-22 14:35:10 -0500

    Chapter 34 Networking. Add fetching data from web service using synchronous network request.

--- StoreSearch12/SearchViewController.swift
+++ StoreSearch12/SearchViewController.swift
@@ -15,6 +15,14 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
     
+    //Cell Reuse identifiers
+        struct TableView {
+            struct CellIdentifiers {
+                static let searchResultCell = "SearchResultCell"
+                static let nothingFoundCell = "NothingFoundCell"
+            }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //Gives the search bar room to breath.
@@ -34,35 +42,85 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
         print("This is the fucking console")
     }
-//Cell Reuse identifiers
-    struct TableView {
-        struct CellIdentifiers {
-            static let searchResultCell = "SearchResultCell"
-            static let nothingFoundCell = "NothingFoundCell"
+
+// MARK: - Helper Methods
+
+    //Create the URL for the request.
+    func iTunesURL(searchText: String) -> URL {
+//enables the search to handle special characters
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@",
+                             encodedText)
+      let url = URL(string: urlString)
+    
+      return url!
+    }
+
+    //Returns a new JSON Data object from the server.
+    func performStoreRequest(with url:URL) -> Data? {
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            print("Download Error: \(error.localizedDescription)")
+            showNetworkError()
+            return nil
         }
     }
 
+    //Use a JSONdecoder to convert the data to a ResultArray object to extract the results property.
+    func parse(data: Data) -> [SearchResult] {
+      do {
+        let decoder = JSONDecoder()
+        let result = try decoder.decode(ResultArray.self,
+                                        from: data)
+        return result.results
+      } catch {
+        print("JSON Error: \(error)")
+        return []
+      }
+    }
+    
+    //Error Alert
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Whoops...",
+                                      message: "There was an error accessing the iTunes Store." + " Please try again.",
+                                      preferredStyle: .alert)
+      
+        let action = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        
+        present(alert,
+              animated: true,
+              completion: nil)
+    }
+    
 }
 
 //MARK:- Seach Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    searchBar.resignFirstResponder()
-          searchResults = []
-        
-        if searchBar.text! != "justin bieber" {
-        for i in 0...2 {
-            let searchResult = SearchResult()
-            searchResult.name = String(format: "Fake Result %d for",
-                                       i)
-            searchResult.artistName = searchBar.text!
-            searchResults.append(searchResult)
-          }
+//None of this will happen unless the user types something into the search field.
+        if !searchBar.text!.isEmpty {
+          searchBar.resignFirstResponder()
+            hasSearched = true
+            searchResults = []
+
+            let url = iTunesURL(searchText: searchBar.text!)
+            print("URL: '\(url)'")
+            
+            if let data = performStoreRequest(with: url) {
+                
+                searchResults = parse(data: data)
+ //Sorts the results
+                searchResults.sort(by: < )
+            }
+            tableView.reloadData()
         }
-    print("The search text is: '\(searchBar.text!)'")
-        hasSearched = true
-          tableView.reloadData()
-        }
+      }
     
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
@@ -94,9 +152,18 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
       } else {
         let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell,
                                                  for: indexPath) as! SearchResultCell
-
+//Puts the artists name into the name label.
         let searchResult = searchResults[indexPath.row]
         cell.nameLabel.text = searchResult.name
+            //Check to see if the artist is not empty. If it is, then display unknown.
+        if searchResult.artist.isEmpty {
+          cell.artistNameLabel.text = "Unknown"
+        } else {
+          cell.artistNameLabel.text = String(
+            format: "%@ (%@)",
+            searchResult.artist,
+            searchResult.type)
+        }
         cell.artistNameLabel.text = searchResult.artistName
         return cell
       }
